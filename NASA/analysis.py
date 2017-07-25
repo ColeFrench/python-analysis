f460166d584d342149a7f265747ecfb8567f34cd
Cole French <ColeFrench@users.noreply.github.com>
2017-07-25 17:12:48 -0400

    Begin analyzing NASA data

---
+++ NASA/analysis.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+import sys
+import matplotlib.pyplot as plt
+
+
+def main():
+    months = {
+        'Jan': 1,
+        'Feb': 2,
+        'Mar': 3,
+        'Apr': 4,
+        'May': 5,
+        'Jun': 6,
+        'Jul': 7,
+        'Aug': 8,
+        'Sep': 9,
+        'Oct': 10,
+        'Nov': 11,
+        'Dec': 12
+    }
+    years = []
+    changes = []
+
+    with open('GLB.Ts.txt') as data:
+        reading = False
+        for line in data:
+            if reading:
+                if line.find('Divide') == 0:
+                    break
+
+                split = line.split()
+                if len(split) >= 2 and line.find('Year') != 0:
+                    years.append(split[0])
+                    changes.append(split[months[sys.argv[1]]])
+
+            elif line.find('Year') == 0:
+                reading = True
+
+    plt.plot(years, changes)
+    plt.title('Changes in Temperature by Hundredths of Degrees Celsius in {} Over Time'.format(
+        sys.argv[1]))
+    plt.xlabel('Year')
+    plt.ylabel('Change in Temperature (Hundredth of a Degree Celsius)')
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
